7328ba0f458ac2cead6a1c30cfa459ebabba54ce
Amit <amit.singh@yatra.com>
2019-08-09 21:49:16 +0530

    Initial Commit

---
+++ SwiftyService/SwiftyService.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyService.h
+//  SwiftyService
+//
+//  Created by Amit Singh on 09/08/19.
+//  Copyright © 2019 Amit Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyService.
+FOUNDATION_EXPORT double SwiftyServiceVersionNumber;
+
+//! Project version string for SwiftyService.
+FOUNDATION_EXPORT const unsigned char SwiftyServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyService/PublicHeader.h>
+
+
